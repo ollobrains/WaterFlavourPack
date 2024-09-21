4008ada8479c5de60d7c9b9ba9d649d69e28f321
Kieran David Evans <keyz182@gmail.com>
2024-09-21 12:58:07 +0100

    Patches for allowing hemogen extraction on hemogenic pawns, and to prevent application of bloodloss on extraction

---
+++ 1.5/Source/Thirst/MSS_ThirstDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSS_Thirst;
+
+[DefOf]
+public static class MSS_ThirstDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+
+    static MSS_ThirstDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(MSS_ThirstDefOf));
+}
---
+++ 1.5/Source/Thirst/MSS_ThirstMod.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Verse;
+using UnityEngine;
+using HarmonyLib;
+using RimWorld;
+
+namespace MSS_Thirst;
+
+public class MSS_ThirstMod : Mod
+{
+    public MSS_ThirstMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from MSS_Thirst");
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("MSS.Thirst.main");	
+        harmony.PatchAll();
+        
+        // FieldInfo BarTex = Assembly.GetAssembly(typeof(GeneGizmo_ResourceHemogen))
+        //     .GetType("RimWorld.GeneGizmo_ResourceHemogen")
+        //     .GetField("BarTex", BindingFlags.NonPublic | BindingFlags.Static);
+        // BarTex?.SetValue(null, SolidColorMaterials.NewSolidColorTexture(new Color(0f, 0.6f, 1)));
+        
+        // FieldInfo barTex = AccessTools.Field(typeof(GeneGizmo_ResourceHemogen), "barTex");
+        //
+        // barTex?.SetValue(null, SolidColorMaterials.NewSolidColorTexture(new Color(0f, 0.6f, 1)));
+    }
+}
