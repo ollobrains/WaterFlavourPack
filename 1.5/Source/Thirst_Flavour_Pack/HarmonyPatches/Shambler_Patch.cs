38fc889d8a38c738cd27d93c9143bb192a49b4f5
Michał Wisiński <wisinski@mail.ru>
2024-09-30 11:12:53 +0200

    Tweaked the colour a bit more

--- 1.5/Source/Thirst_Flavour_Pack/HarmonyPatches/Shambler_Patch.cs
+++ 1.5/Source/Thirst_Flavour_Pack/HarmonyPatches/Shambler_Patch.cs
@@ -14,8 +14,8 @@ public class ShamblerColorPatch
         Color.RGBToHSV(__result, out float H, out float S, out float V);
         // Change the stauration to make it a light grey
         H = Rand.RangeSeeded(0.08f, 0.12f, __result.GetHashCode());
-        S = Rand.RangeSeeded(0.4f, 0.6f, __result.GetHashCode());
-        V = Rand.RangeSeeded(0.7f, 1.0f, __result.GetHashCode());
+        S = Rand.RangeSeeded(0.5f, 0.9f, __result.GetHashCode());
+        V = Rand.RangeSeeded(0.6f, 1.0f, __result.GetHashCode());
         return Color.HSVToRGB(H, S, V);
     }
 }
