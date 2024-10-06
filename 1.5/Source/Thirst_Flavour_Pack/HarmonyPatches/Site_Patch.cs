56ccd9873e68f5a09609935ccba9e2605d81b3aa
Kieran David Evans <keyz182@gmail.com>
2024-10-06 20:03:06 +0100

    Trade requests and prevent sites despawning

---
+++ 1.5/Source/Thirst_Flavour_Pack/HarmonyPatches/Site_Patch.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace Thirst_Flavour_Pack.HarmonyPatches;
+
+[HarmonyPatch(typeof(Site))]
+public static class Site_Patch
+{
+    public static List<SitePartDef> SitesToNotDespawn = [
+        Thirst_Flavour_PackDefOf.MSS_Thirst_ArchospringSite,
+        Thirst_Flavour_PackDefOf.MSS_Thirst_Archospring_PowerRegulator_Site,
+        Thirst_Flavour_PackDefOf.MSS_Thirst_Archospring_CatalyticSeparator_Site,
+        Thirst_Flavour_PackDefOf.MSS_Thirst_Archospring_SterilizationPlant_Site
+    ];
+
+    [HarmonyPatch(nameof(Site.ShouldRemoveMapNow))]
+    [HarmonyPostfix]
+    public static void ShouldRemoveMapNow_Postfix(Site __instance, ref bool __result)
+    {
+        // Stops our sites being auto-removed when caravanning away
+        if (__instance.parts.Any(p => SitesToNotDespawn.Contains(p.def)))
+        {
+            __result = false;
+        }
+    }
+
+}
